31cec102dbde13326071e5052d92a709331ef860
turaabhaider <turaab.haider0110@gmail.com>
2026-03-11 11:39:49 -0700

    origin

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 const server = http.createServer(app);
 
 const io = new Server(server);
